4d8013a7bf9e64c809dceb875273e7907dc0ccb6
Ricardo Sanchez <odrakir@gmail.com>
2019-07-10 11:21:55 +0200

    Use toggl addresses

--- Toggl.Networking.Tests.Integration/Helper/RandomEmail.cs
+++ Toggl.Networking.Tests.Integration/Helper/RandomEmail.cs
@@ -6,9 +6,9 @@ namespace Toggl.Networking.Tests.Integration.Helper
     public static class RandomEmail
     {
         public static Email GenerateValid()
-            => Email.From($"{Guid.NewGuid()}@mocks.toggl.com");
+            => Email.From($"{Guid.NewGuid()}@toggl.com");
 
         public static Email GenerateInvalid()
-            => Email.From($"non-existing-email-{Guid.NewGuid()}@ironicmocks.toggl.com");
+            => Email.From($"non-existing-email-{Guid.NewGuid()}@toggl.com");
     }
 }
