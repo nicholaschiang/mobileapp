b8ae1348545e7d61dde7bbbc032d7179d27c6130
Ricardo Sanchez <odrakir@gmail.com>
2019-08-27 17:14:04 +0200

    Change text colors

--- Toggl.iOS/Extensions/ColorAssets.cs
+++ Toggl.iOS/Extensions/ColorAssets.cs
@@ -8,13 +8,14 @@ namespace Toggl.iOS.Extensions
         public static readonly UIColor AlternateBackground = UIColor.FromName("AlternateBackground");
         public static readonly UIColor Separator = UIColor.FromName("Separator");
         public static readonly UIColor Text = UIColor.FromName("Text");
-        public static readonly UIColor PlaceholderText = UIColor.FromName("PlaceholderText");
-        public static readonly UIColor FadedText = UIColor.FromName("FadedText");
+        public static readonly UIColor Text2 = UIColor.FromName("Text2");
+        public static readonly UIColor Text3 = UIColor.FromName("Text3");
+        public static readonly UIColor Text4 = UIColor.FromName("Text4");
+
 
         public static class Timeline
         {
             public static readonly UIColor ExpandedGroup = UIColor.FromName("Timeline_ExpandedGroup");
-            public static readonly UIColor CollapsedGroup = UIColor.FromName("Timeline_CollapsedGroup");
             public static readonly UIColor Arrow = UIColor.FromName("Timeline_Arrow");
         }
     }
